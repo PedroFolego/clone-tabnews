e5207c3ee9a1fdbfd262f10556677bc3ea71035b
Pedro Folego <91345036+PedroFolego@users.noreply.github.com>
2024-05-23 19:40:50 +0000

    infra: add .env and database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from '../../../../infra/database.js';
+
+async function status(req, res) {
+  const data = await database.query('SELECT 1 + 1;')
+  console.log(data);
   res.status(200).json({ bararara: 'burrururururururu' })
 }
 
