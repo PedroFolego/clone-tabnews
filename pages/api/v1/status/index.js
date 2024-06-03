25a763b89baa07e5dfe6556c42785ee61fdf378b
Pedro Folego <91345036+PedroFolego@users.noreply.github.com>
2024-06-03 18:23:24 +0000

    config: add jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,7 @@
-import database from '../../../../infra/database.js';
+import database from 'infra/database.js';
 
 async function status(req, res) {
   const data = await database.query('SELECT 1 + 1;')
-  console.log(data);
   res.status(200).json({ bararara: 'burrururururururu' })
 }
 
