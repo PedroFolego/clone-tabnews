c2efcb30cfa77f92d08ad9ac741238dc01e35867
Pedro Folego <91345036+PedroFolego@users.noreply.github.com>
2024-08-08 21:11:21 +0000

    testing postgress queries

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,28 @@
 import database from 'infra/database.js';
 
 async function status(req, res) {
-  const data = await database.query('SELECT 1 + 1;')
-  res.status(200).json({ bararara: 'burrururururururu' })
+  const updatedAt = new Date().toISOString()
+  const dataBaseVersionResult = await database.query('SHOW server_version;')
+  const databaseVersionValue = dataBaseVersionResult.rows[0].server_version
+
+  const dataBaseMaxConnectionResult = await database.query('SHOW max_connections;')
+  const databaseMaxConnectionValue = dataBaseMaxConnectionResult.rows[0].max_connections
+
+  const dataBaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';")
+  const databaseOpenedConnectionsValue = dataBaseOpenedConnectionsResult.rows[0].count
+  console.log(databaseOpenedConnectionsValue);
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opened_connections: databaseOpenedConnectionsValue,
+
+      }
+    }
+  })
 }
 
 export default status
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test('Get to /api/v1/status should return 200', async () => {
   const res = await fetch('http://localhost:3000/api/v1/status')
   expect(res.status).toBe(200)
+
+  const responseBody = await res.json()
+  expect(responseBody.updated_at).toBeDefined()
+
+  expect(responseBody.dependencies.database.version).toEqual('16.3')
+  expect(responseBody.dependencies.database.max_connections).toEqual(100)
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1)
+
 })
