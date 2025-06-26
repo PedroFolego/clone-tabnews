545ed52aecfc631fb8cd9eeb8b82ba005c5f98a8
Pedro Folego <hmfpedro@hotmail.com>
2025-06-26 11:20:51 -0300

    fix: prettier style

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,22 +1,23 @@
-import database from 'infra/database.js';
+import database from "infra/database.js";
 
 async function status(req, res) {
-  const updatedAt = new Date().toISOString()
-  const dataBaseVersionResult = await database.query('SHOW server_version;')
-  const databaseVersionValue = dataBaseVersionResult.rows[0].server_version
+  const updatedAt = new Date().toISOString();
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = dataBaseVersionResult.rows[0].server_version;
 
-  const dataBaseMaxConnectionResult = await database.query('SHOW max_connections;')
-  const databaseMaxConnectionValue = dataBaseMaxConnectionResult.rows[0].max_connections
+  const dataBaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue =
+    dataBaseMaxConnectionResult.rows[0].max_connections;
 
-
-  const databaseName = process.env.POSTGRES_DB
+  const databaseName = process.env.POSTGRES_DB;
   const dataBaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName]
-  }
-
-  )
-  const databaseOpenedConnectionsValue = dataBaseOpenedConnectionsResult.rows[0].count
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count;
 
   res.status(200).json({
     updated_at: updatedAt,
@@ -25,10 +26,9 @@ async function status(req, res) {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionValue),
         opened_connections: databaseOpenedConnectionsValue,
-
-      }
-    }
-  })
+      },
+    },
+  });
 }
 
-export default status
+export default status;
--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,17 +1,17 @@
-import database from "infra/database.js"
-import orchestrator from "tests/orchestrator.js"
+import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await orchestrator.waitForAllServices()
-  await database.query('drop schema public cascade; create schema public')
-})
+  await orchestrator.waitForAllServices();
+  await database.query("drop schema public cascade; create schema public");
+});
 
-test('Get to /api/v1/migrations should return 200', async () => {
-  const res = await fetch('http://localhost:3000/api/v1/migrations')
-  expect(res.status).toBe(200)
+test("Get to /api/v1/migrations should return 200", async () => {
+  const res = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(res.status).toBe(200);
 
-  const responseBody = await res.json()
+  const responseBody = await res.json();
 
-  expect(Array.isArray(responseBody)).toBe(true)
-  expect(responseBody.length).toBeGreaterThan(0)
-})
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
